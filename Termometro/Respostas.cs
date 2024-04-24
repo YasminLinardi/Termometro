2b3285e860b1e270e99209f2ca1f7b9eb17dd4b3
Termometro <yasminvitorialinardi@gmail.com>
2024-04-24 19:07:39 -0300

    tanto faz

---
+++ Termometro/Respostas.cs
@@ -0,0 +1,7 @@
+namespace Termometro;
+
+public class Resposta
+
+{
+    public Results ressults {get;set;}
+}
--- Termometro/Results.cs
+++ Termometro/Results.cs
@@ -7,7 +7,36 @@ public class Results
 
     public int Humidity {get;set;}
 
+    public double Rain {get;set;}
+
     public string Description {get;set;}
 
+    public string Img_id {get;set;}
+
+    public string Currently {get;set;}
+
+    public string Cid {get;set;}
+
+    public string City {get;set;}
+
+    public string Sunrise {set;get;}
+
+    public string Sunset {set;get;}
+
+    public string Wind_speedy {get;set;}  
+
+    public string Wind_direction {get;set;}
+
+    public string Wind_cardinal {get;set;}
+
+    public string Moon_phase {get;set;}
+
+    public string Condition_slug {get;set;}
+
+    public string City_name {get;set;}
+
+    public string Timezone {get;set;}
+
+    public string Forecast {get;set;}
 
 }
